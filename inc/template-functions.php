ed0a2b9eaf1309bf1bbb3e34a6b4068267b67a99
gltsagar <glt.sagarshrestha@gmail.com>
2024-10-27 12:12:00 +0545

    Removed header part

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -55,12 +55,53 @@ if ( ! function_exists( 'constructionn_header_inclusion' ) ) :
 	 * Header Function
 	 */
 	function constructionn_header_inclusion() {
-		$header_array = array( 'one', 'two', 'three', 'four', 'five' );
-		$header       = get_theme_mod( 'header_layouts', 'one' );
-
-		if ( in_array( $header, $header_array ) ) {
-			get_template_part( 'headers/' . $header );
-		}
+		$toggle_header = get_theme_mod( 'topbar_toggle', true ); ?>
+		<div class="site-header style-one">
+			<div class="header-top">
+				<div class="container">
+					<div class="header-top-wrapper">
+						<?php constructionn_header_message(); ?>
+						<div class="top-right">
+							<?php constructionn_secondary_nagivation(); ?>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="desktop-header">
+				<?php if ( $toggle_header ) { ?>
+					<div class="container">
+						<div class="header-wrapper">
+							<div class="logo-wrapper">
+								<?php constructionn_site_branding( true ); ?>
+							</div>
+							<div class="primary-menu-wrapper">
+								<?php constructionn_primary_nagivation(); ?>
+							</div>
+							<div class="header-right">
+								<ul class="header-link-list">
+									<li class="search-wrap">
+										<a href="#" id="headerSearchBtn">
+											<span class="search-icon">
+												<?php echo wp_kses( constructionn_handle_all_svgs( 'header-search-icon' ), constructionn_get_kses_extended_ruleset() ); ?>
+											</span>
+											<span class="close-icon">
+												<?php echo wp_kses( constructionn_handle_all_svgs( 'header-search-icon-close' ), constructionn_get_kses_extended_ruleset() ); ?>
+											</span>
+										</a>
+										<div class="search-wrapper" id="headerSearch">
+											<?php get_search_form(); ?>
+										</div>
+									</li>
+										<?php constructionn_front_header_one_request_quote(); ?>
+								</ul>
+							</div>
+						</div>
+					</div>
+				<?php } ?>
+			</div>
+				<?php constructionn_mobile_header(); ?>
+		</div>
+	<?php	
 	}
 endif;
 add_action( 'constructionn_header', 'constructionn_header_inclusion', 10 );
